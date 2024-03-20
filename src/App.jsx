9fdb5a1b60edf36f6327f030cffd0afad221779f
Vanshika0629 <snnvanshika1524@gmail.com>
2024-03-20 17:46:56 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import Cardcode from './Cardcode'
+import Cardcode from "./Cardcode.jsx"
+import Home from "./Home.jsx"
 export default function App() {
   return (
         <div>
+          <Home/>
           <Cardcode/>
         </div>
   )
--- src/Cardcode.jsx
+++ src/Cardcode.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Card from "react-bootstrap/Card";
 import style from "./CardCss.module.css";
 import { Col, Container, Row } from "react-bootstrap";
@@ -5,36 +6,48 @@ import { Col, Container, Row } from "react-bootstrap";
 export default function Cardcode() {
   return (
     <>
-    <div class="card">
-      <Row className="p-5">
-        <Col className="pt-4">
-          <h1>Enjoy on your TV</h1>
-          <p className="pt-3">
-            {" "}
-            Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray
-            players and more.{" "}
-          </p>
-        </Col>
-        <Col
-          style={{
-            display: "flex",
-            alignContent: "right",
-            justifyContent: "right",
-          }}
-        >
-          <img
-            className="p-0"
-            height={350}
-            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
-          />
-          <video
-            style={{ position: "absolute" }}
-            autoPlay
-            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
-          />
-        </Col>
-      </Row>
-    </div>
+      <div
+        style={{ backgroundColor: "black", color: "white" }}
+        className="card"
+      >
+        <Row className="p-5">
+          <Col className="pt-5">
+            <h1 style={{fontSize:"38px",fontStyle:"bold"}}>Enjoy on your TV</h1>
+            <p style={{fontSize:"27px"}}className="pt-3">
+              {" "}
+              Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+              Blu-ray players and more.{" "}
+            </p>
+          </Col>
+          <Col
+            style={{
+              display: "flex",
+              alignContent: "right",
+              justifyContent: "right",
+            }}
+          >
+            <img
+              className="p-0"
+              height={350}
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+            />
+            <video
+              style={{
+                position: "absolute",
+                alignItems: "left",
+                justifyContent: "left",
+                marginTop: "75px",
+                marginBottom: "75px",
+                marginLeft: "70px",
+                marginRight: "70px",
+                height: "34vh",
+              }}
+              autoPlay
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
+            />
+          </Col>
+        </Row>
+      </div>
     </>
   );
 }

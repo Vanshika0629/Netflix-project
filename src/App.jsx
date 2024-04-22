5b94bc6caeed3ba2282be554a97e8ed2301e3477
Vanshika0629 <snnvanshika1524@gmail.com>
2024-04-22 10:53:41 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import React from 'react'
 import Front from "./Front.jsx";
-import Signup from './Signup.jsx';
-import Sign from './Sign.jsx';
+import Signup from "./Signup.jsx";
+import Sign from "./Sign.jsx";
+import Home from "./Home.jsx";
+import Vplayer from "./Vplayer.jsx";
+import Account from "./Account.jsx";
+import Tvmovie from "./Tvmovie.jsx";
+import Member from "./Member.jsx";
+import Profiles from "./Profiles.jsx";
+import Changeplan from "./Changeplan.jsx";
+import Managepayment from "./Managepayment.jsx";
+import Changepassword from "./Changepassword.jsx";
+import Billingdetails from "./Billingdetails.jsx";
 
 export default function App() {
   return(
@@ -11,6 +21,17 @@ export default function App() {
       <Route path='/' element={<Front/>}/>
       <Route path='/signin' element={<Sign/>}/>
       <Route path='/signup' element={<Signup/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/acc' element={<Account/>}/>
+      <Route path='/member' element={<Member/>}/>
+      <Route path='/pro' element={<Profiles/>}/>
+      <Route path='/play' element={<Vplayer/>}/>
+      <Route path='/tv' element={<Tvmovie/>}/>
+      <Route path='/cplan' element={<Changeplan/>}/>
+      <Route path='/managepayment' element={<Managepayment/>}/>
+      <Route path='/changepassword' element={<Changepassword/>}/>
+      <Route path='/billingdetails' element={<Billingdetails/>}/>
+
     </Routes>
     </BrowserRouter>    
   )
